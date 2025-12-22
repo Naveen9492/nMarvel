84d6301d5169e44298097470039e678a13696d18
Naveen <knvalwayslike7@gmail.com>
2025-12-23 01:45:33 +0530

    prepare for deploy

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,54 +1,86 @@
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
-import './index.css';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import "./index.css";
 
 const NavBar = () => {
   const [open, setOpen] = useState(false);
 
   return (
     <nav className="navbar-container">
-      <Link to="/" className="nav-item-link" >
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Marvel_Logo.svg"
-        alt="navbar-logo"
-        className="logo-image"
-      />
+      <Link to="/" className="nav-item-link">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Marvel_Logo.svg"
+          alt="navbar-logo"
+          className="logo-image"
+        />
       </Link>
 
       <div className="hamburger" onClick={() => setOpen(!open)}>
-        {!open ? <FontAwesomeIcon icon={faBars} /> : <FontAwesomeIcon icon={faXmark} />}
+        {!open ? (
+          <FontAwesomeIcon icon={faBars} />
+        ) : (
+          <FontAwesomeIcon icon={faXmark} />
+        )}
       </div>
 
-      <div className={`navbar-menu-item-container ${open ? 'open' : ''}`}>
+      <div className={`navbar-menu-item-container ${open ? "open" : ""}`}>
         <Link to="/" onClick={() => setOpen(false)} className="nav-item-link">
           <p className="navbar-menu-item">HOME</p>
         </Link>
-        <Link to="/movies" onClick={() => setOpen(false)} className="nav-item-link">
+        <Link
+          to="/movies"
+          onClick={() => setOpen(false)}
+          className="nav-item-link"
+        >
           <p className="navbar-menu-item">MOVIES</p>
         </Link>
-        <Link to="/comics" onClick={() => setOpen(false)} className="nav-item-link">
+        <Link
+          to="/comics"
+          onClick={() => setOpen(false)}
+          className="nav-item-link"
+        >
           <p className="navbar-menu-item">COMICS</p>
         </Link>
-        <Link to="/characters" onClick={() => setOpen(false)} className="nav-item-link">
+        <Link
+          to="/characters"
+          onClick={() => setOpen(false)}
+          className="nav-item-link"
+        >
           <p className="navbar-menu-item">CHARACTERS</p>
         </Link>
-        <Link to="/tvshows" onClick={() => setOpen(false)} className="nav-item-link">
+        <Link
+          to="/tvshows"
+          onClick={() => setOpen(false)}
+          className="nav-item-link"
+        >
           <p className="navbar-menu-item">TV SHOWS</p>
         </Link>
-        <Link to="/news" onClick={() => setOpen(false)} className="nav-item-link">
+        <Link
+          to="/news"
+          onClick={() => setOpen(false)}
+          className="nav-item-link"
+        >
           <p className="navbar-menu-item">NEWS</p>
         </Link>
-        <Link to="/games" onClick={() => setOpen(false)} className="nav-item-link">
+        <Link
+          to="/games"
+          onClick={() => setOpen(false)}
+          className="nav-item-link"
+        >
           <p className="navbar-menu-item">GAMES</p>
         </Link>
-        <Link to="/videos" onClick={() => setOpen(false)} className="nav-item-link">
+        <Link
+          to="/videos"
+          onClick={() => setOpen(false)}
+          className="nav-item-link"
+        >
           <p className="navbar-menu-item">VIDEOS</p>
         </Link>
       </div>
     </nav>
-  )
-}
+  );
+};
 
 export default NavBar;
--- src/Components/Trailer/index.js
+++ src/Components/Trailer/index.js
@@ -11,10 +11,10 @@ const Trailer = (props) => {
         height="315"
         src={trailerUrl}
         title="YouTube video player"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
       ></iframe>
     </div>
   );
